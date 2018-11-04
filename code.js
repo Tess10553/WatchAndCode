2b3f64dd0f666e19adbc44052af79dd9a3363139
Tess10553 <lt.tess.yu@gmail.com>
2018-11-04 20:49:21 +0800

    V4 complete

--- code.js
+++ code.js
@@ -1,39 +1,27 @@
-// var todoList = ['item 1', 'item 2', 'item 3']
-
-// //to add new
-// function addList(add){
-//   todoList.push(add);
-// }
-// // to change
-// function changeList(tpoic,newContent){
-//   todoList[topic] = newContent
-// }
-// //to delete
-// function deleteList(topic){
-//   todoList.splice(topic,1)
-// }
-// // display list
-// function displayList(){
-//   console.log('Here\'s my' +  mytodoList)
-// }
-
-//put all functions into a subject
-
 var todoList = {
-  list: ['item 1','item 2','item 3'],
+  list: [],
   showList: function(){
     console.log(this.list);
   },
-  addList: function(add){
-    this.list.push(add);
+  addList: function(text){
+    this.list.push({
+      addText: text,
+      completed: false,
+    })
     this.showList();
   },
   changeList: function(item,change){
-    this.list[item] = change;
+    //this.list[item] = change;
+    this.list[item].addText = change;
     this.showList();
   },
   deleteList: function (item) {
     this.list.splice(item,1);
     this.showList();
-  }
+  },
+  toggleCompleted: function(item){
+    var todo = this.list[item];
+    todo.completed = !todo.completed;
+    this.showList();
+  },
 }
