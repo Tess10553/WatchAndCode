1dc269f8a0dd361dba2656aaa52cdc55b2792aa3
Tess10553 <lt.tess.yu@gmail.com>
2018-11-04 10:52:06 +0800

    git init

---
+++ code.js
@@ -0,0 +1,21 @@
+// var todoList = ['item 1', 'item 2', 'item 3']
+
+// //to add new
+// function addList(add){
+//   todoList.push(add);
+// }
+// // to change
+// function changeList(tpoic,newContent){
+//   todoList[topic] = newContent
+// }
+// //to delete
+// function deleteList(topic){
+//   todoList.splice(topic,1)
+// }
+// // display list
+// function displayList(){
+//   console.log('Here\'s my' +  mytodoList)
+// }
+
+//put all functions into a subject
+
